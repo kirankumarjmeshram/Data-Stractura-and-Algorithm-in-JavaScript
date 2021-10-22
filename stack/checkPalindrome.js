dc6dac9265940343348372722c130c2317e41b1f
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-10-22 15:02:12 +0530

    Palindrome problem is done using stack
    step1 created stack
    step 2  put letters of word into stack
    step3 pop off the stack in reverce orde
    step3 comparing two string

---
+++ stack/checkPalindrome.js
@@ -0,0 +1,25 @@
+var letters = [] //this is our stack
+
+var word = 'recerq';
+
+var rword='';
+
+//put letters of word into stack
+
+for (var i=0;i<word.length;i++){
+    letters.push(word[i]);
+}
+
+//pop off the stack in reverce order
+//The pop() method removes the last element from an letters and returns that element to rword
+for ( var i=0;i<word.length;i++){
+    //lettere is object and converting in reverse string
+    rword = rword + letters.pop();
+}
+
+//conparing two string
+if(word === rword){
+    console.log(word + " is a palindrome.");
+}else{
+    console.log(word + " is not a palindrome.");
+}
