c0b281ca87bfde9dddcc9e383cefae0e25516aa0
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-26 13:20:03 +0530

    or '|' operator in test()
    You can search for multiple patterns using the alternation or OR operator: |

--- javaScriptBasic/test.js
+++ javaScriptBasic/test.js
@@ -2,3 +2,12 @@ let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
 console.log(result)//true
+
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+console.log(result)//true
+
+// You can search for multiple patterns using the alternation or OR operator: |
