ffaf9592fc1867a4183537476b094ac57c117026
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-26 08:12:26 +0530

    unshift()

--- javaScriptBasic/genral.js
+++ javaScriptBasic/genral.js
@@ -5,3 +5,11 @@ Array.isArray([1, 2, 3]);  // true
 Array.isArray({foo: 123}); // false
 Array.isArray('foobar');   // false
 Array.isArray(undefined);  // false
+
+
+
+//unshift(...)
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(['Paul',35]);
+console.log(myArray) //o-[ [ 'Paul', 35 ], [ 'dog', 3 ] ]
