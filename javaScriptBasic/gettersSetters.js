2a1d7186d94b9a66c9157e7235ae4d8ca21ba1b1
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-29 08:31:11 +0530

    getters and setters to Control Access to an Object
    Getter functions are meant to simply return (get) the value of an object's private variable to the user without the user directly accessing the private variable.
    
    Setter functions are meant to modify (set) the value of an object's private variable based on the value passed into the setter function. This change could involve calculations, or even overwriting the previous value completely.

--- javaScriptBasic/gettersSetters.js
+++ javaScriptBasic/gettersSetters.js
@@ -1,11 +1,41 @@
-var person ={
-    firstName : 'Kirankumar',
-    lastName : 'J Meshram',
-    //fullName: function(){},
-    fullName(){
-      return  `${person.firstName} ${person.lastName}`
+//getters and setters to Control Access to an Object
+
+//Getter functions are meant to simply return (get) the value of an object's private variable 
+//to the user without the user directly accessing the private variable.
+//Setter functions are meant to modify (set) the value of an object's private variable 
+//based on the value passed into the setter function. 
+//This change could involve calculations, or even overwriting the previous value completely.
+
+
+class Book {
+    constructor(author) {
+      this._author = author;
     }
-};
+    // getter
+    get writer() {
+      return this._author;
+    }
+    // setter
+    set writer(updatedAuthor) {
+      this._author = updatedAuthor;
+    }
+  }
+  const novel = new Book('anonymous');
+  console.log(novel.writer);
+  novel.writer = 'newAuthor';
+  console.log(novel.writer);
+  
+
+
+
+// var person ={
+//     firstName : 'Kirankumar',
+//     lastName : 'J Meshram',
+//     //fullName: function(){},
+//     fullName(){
+//       return  `${person.firstName} ${person.lastName}`
+//     }
+// };
 //console.log(person.firstName+ ' ' + person.lastName);
 //console.log(`${person.firstName} ${person.lastName}`);
 //console.log(person.fullName()); this is readonly. ie we cannot change property from outside
@@ -30,6 +60,8 @@ var person ={
 person.fullName = 'krishn kumar'
 
 console.log(person)//-o { firstName: 'krishn', lastName: 'kumar', fullName: [Getter/Setter] }
+
+
 
 
 
