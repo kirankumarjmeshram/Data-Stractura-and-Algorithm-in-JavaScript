5bf08244421aa2357bfd4a8a8502f692e46e6081
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-29 19:14:28 +0530

    getters and setters

--- javaScriptBasic/gettersSetters.js
+++ javaScriptBasic/gettersSetters.js
@@ -21,12 +21,29 @@ class Book {
     }
   }
   const novel = new Book('anonymous');
-  console.log(novel.writer);
+  console.log(novel.writer);//anonymous 
   novel.writer = 'newAuthor';
-  console.log(novel.writer);
+  console.log(novel.writer);//newAuthor
   
 
+  class Thermostat{
+    constructor(fahrenheit){
+      this.fahrenheit = fahrenheit;
+    }
+    get temperature(){
+      return (5/9)*(this.fahrenheit-32)
+    }
+    set temperature(celsius){
+      this.fahrenheit = (celsius*9)/5+32;
+    }
+  }
 
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
+  
 
 // var person ={
 //     firstName : 'Kirankumar',
