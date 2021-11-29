398d60bea7b3170559715f3e8dbec10f3b9572a8
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-29 18:42:20 +0530

    constructor function

---
+++ javaScriptBasic/constroctorFunction.js
@@ -0,0 +1,16 @@
+function GeneralCar(givenName,givenSpeed){
+    this.name = givenName;
+    this.speed =givenSpeed;
+
+    this.run = function(){
+        console.log(`${this.name} is running`)
+    };
+    this.analyze = function(){
+        console.log(`This car is slower by ${200-this.speed} than Porshe`)
+    }
+}
+
+car1 = new GeneralCar('Nissan',180);
+car2 = new GeneralCar("Maruti Alto",89);
+
+console.log(car1.analyze())
