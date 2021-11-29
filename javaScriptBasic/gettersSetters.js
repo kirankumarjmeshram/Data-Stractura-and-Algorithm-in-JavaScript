5b2f18d0ac85124abef5826f1ab0226ebadc7f8e
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-11-29 08:25:03 +0530

    getters and setters

--- javaScriptBasic/gettersSetters.js
+++ javaScriptBasic/gettersSetters.js
@@ -1,7 +1,35 @@
-// var person ={
-//     firstName : 'Kirankumar',
-//     lastName : 'J Meshram'
+var person ={
+    firstName : 'Kirankumar',
+    lastName : 'J Meshram',
+    //fullName: function(){},
+    fullName(){
+      return  `${person.firstName} ${person.lastName}`
+    }
+};
+//console.log(person.firstName+ ' ' + person.lastName);
+//console.log(`${person.firstName} ${person.lastName}`);
+//console.log(person.fullName()); this is readonly. ie we cannot change property from outside
 
-// };
-// console.log(person.firstName+ ' ' + person.lastName);
- console.log("Hello")
+//getters => access properties
+//setters => change(mutate) them
+
+var person ={
+    firstName : 'Kirankumar',
+    lastName : 'J Meshram',
+    //fullName: function(){},
+    get fullName(){
+      return  `${person.firstName} ${person.lastName}`
+    },
+    set fullName(value){
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+};
+
+person.fullName = 'krishn kumar'
+
+console.log(person)//-o { firstName: 'krishn', lastName: 'kumar', fullName: [Getter/Setter] }
+
+
+
