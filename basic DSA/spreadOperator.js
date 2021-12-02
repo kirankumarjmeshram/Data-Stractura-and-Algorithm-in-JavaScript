b34a9636d3e05d3ac38eedee2f9268651cffb2db
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-02 20:31:12 +0530

    Combine Arrays with the Spread Operator
     this only allows us to combine arrays at the end of one, and at the start of another.

--- basic DSA/spreadOperator.js
+++ basic DSA/spreadOperator.js
@@ -20,3 +20,17 @@ function copyMachine(arr, num) {
   //   [ true, false, true ],
   //   [ true, false, true ] ]
 
+//Combine Arrays with the Spread Operator
+let thisArray1 = ['sage', 'rosemary', 'parsley', 'thyme'];
+let thatArray1 = ['basil', 'cilantro', ...thisArray, 'coriander'];
+console.log(thatArray1)//=> ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+
+
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence=['learning',...fragment,'is','fun']; // Change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());//[ 'learning', 'to', 'code', 'is', 'fun' ]
+
