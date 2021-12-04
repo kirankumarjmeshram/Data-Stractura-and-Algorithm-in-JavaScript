48f8fe7d1a649e02fe03593f3b19e73d628835a5
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-04 08:40:22 +0530

    Bubble Sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,21 @@
+function bubbleSort(array) {
+   
+    for(var i=0;i<array.length;i++){
+      for(var j=0;j<array.length-i;j++){
+        if(array[j]>array[j+1]){
+          var tempt;
+          tempt=array[j];
+          array[j]=array[j+1];
+          array[j+1]= tempt;
+  
+        }else{
+          continue;
+        }
+      }
+    }
+  
+    console.log(array) ;
+    
+  }
+  array=[1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92];
+  bubbleSort(array)
