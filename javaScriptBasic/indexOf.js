e3fc4d71b160339a651a017256be968b15319119
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-23 18:28:34 +0530

    indexOf()
    Check For The Presence of an Element With indexOf()

---
+++ javaScriptBasic/indexOf.js
@@ -0,0 +1,7 @@
+let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+
+var a= fruits.indexOf('dates');//-1= element does not exist
+var b= fruits.indexOf('oranges');//2
+var c= fruits.indexOf('pears');//1
+
+console.log(a,b,c)
