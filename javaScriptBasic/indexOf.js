1b440e25cfe789b17b737b2f0bff0b412ef4be57
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-23 18:32:39 +0530

    Update indexOf.js

--- javaScriptBasic/indexOf.js
+++ javaScriptBasic/indexOf.js
@@ -5,3 +5,17 @@ var b= fruits.indexOf('oranges');//2
 var c= fruits.indexOf('pears');//1
 
 console.log(a,b,c)
+
+function quickCheck(arr, elem) {
+    // Only change code below this line
+  if(arr.indexOf(elem)>=0){
+    return true;
+  }
+  return false;
+  
+  
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'onions'));
