0bea1ec7206c706f4cab000c05679e93bdbf50eb
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-23 19:01:48 +0530

    adding key value to object and updating key

---
+++ javaScriptBasic/addKeyVAluePainToObj.js
@@ -0,0 +1,19 @@
+const tekkenCharacter = {
+    player: 'Hwoarang',
+    fightingStyle: 'Tae Kwon Doe',
+    human: true
+  };
+
+  tekkenCharacter.origin = 'South Korea';
+  tekkenCharacter['hair color'] = 'dyed orange';
+
+  tekkenCharacter.player = 'Dhoni'// change value of key
+  console.log(tekkenCharacter)
+  /*{
+  player: 'Hwoarang',
+  fightingStyle: 'Tae Kwon Doe',
+  human: true,
+  origin: 'South Korea',
+  'hair color': 'dyed orange'
+}
+ */
