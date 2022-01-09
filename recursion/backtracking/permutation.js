4bbe198456273cb880d17ee85d042b51750f775d
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-09 17:36:59 +0530

    permutation

---
+++ recursion/backtracking/permutation.js
@@ -0,0 +1,29 @@
+
+
+function getPermutation(array){
+    var permutations = [];
+    permutationsHelper(0,array,permutations);
+    return permutations;
+}
+
+function permutationsHelper(i,array,permutations){
+    if(i=== array.length){
+        permutations.push(array.slice())
+    }else{
+        for(var j=i;j<array.length;j++){
+            swap(i,j,array);
+            permutationsHelper(i+1,array,permutations);
+            swap(i,j,array)
+        }
+    }
+}
+
+function swap(i,j,array){
+    var temp=array[i];
+    array[i]=array[j];
+    array[j]=temp;
+}
+
+console.log(getPermutation([1,2,3]))//output=>[ [ 1, 2, 3 ],[ 1, 3, 2 ], [ 2, 1, 3 ],[ 2, 3, 1 ][ 3, 2, 1 ],[ 3, 1, 2 ]]
+
+console.log(getPermutation(["a","b"]))//output=>[ [ 'a', 'b' ], [ 'b', 'a' ] ]
