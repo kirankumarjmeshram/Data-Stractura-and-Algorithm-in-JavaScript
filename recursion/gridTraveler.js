c1abf002cc54b708734a5b1bfd79aea1241d9741
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-24 09:25:47 +0530

    grid Traveler
    The problem is to count all the possible paths from top left to bottom right of a m*n matrix with the constraints that from each cell you can either move only to right or down

---
+++ recursion/gridTraveler.js
@@ -0,0 +1,15 @@
+function gridTravler(m,n){
+    if(m==1 && n==1){
+        return 1
+    }if(m==0 || n==0){
+        return 0
+    }else{
+        return gridTravler(m-1,n)+gridTravler(m,n-1)
+    }
+}
+
+console.log(gridTravler(1,1));//1
+console.log(gridTravler(2,3));//3
+console.log(gridTravler(3,2))//3
+console.log(gridTravler(3,3))//6
+console.log(gridTravler(25,10))//38567100
