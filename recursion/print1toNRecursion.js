f7620b8d22de6b0fa67dea8c1f219bfd104e5b88
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-01-24 08:51:50 +0530

    1 to n

--- recursion/print1toNRecursion.js
+++ recursion/print1toNRecursion.js
@@ -2,17 +2,11 @@
 function printNos(n)
 {
     if(n > 0)
-   
-
     {
         printNos(n - 1);
          console.log(n + " ");
-    
-        
     }
-   
     return;
-    
 }
  
-printNos(10);
+    printNos(10);//1 2 3 4 5 6 7 8 9 10
