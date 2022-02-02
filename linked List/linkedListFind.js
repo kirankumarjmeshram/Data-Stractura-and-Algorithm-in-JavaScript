e4bf694955ade2211e629c9096130db0c2ea8917
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-02-02 17:04:57 +0530

    linked list find

---
+++ linked List/linkedListFind.js
@@ -0,0 +1,34 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d =new Node("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+//A->B->C->D->NULL
+
+const linkedListFind =(head,target)=>{
+    let current = head;
+    while(current!== null){
+        if(current.val === target){
+            return true;
+        }
+        current = current.next
+    }
+    return false
+}
+
+console.log(linkedListFind(a,"C")) //A B C D
+
+
+
+
