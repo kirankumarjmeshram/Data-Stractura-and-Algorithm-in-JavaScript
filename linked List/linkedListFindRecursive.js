7623265aa8a10443aec74e449350dc1f4b7244bc
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-02-02 17:20:48 +0530

    linked list find recursive

---
+++ linked List/linkedListFindRecursive.js
@@ -0,0 +1,29 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d =new Node("D");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+//A->B->C->D->NULL
+
+const linkedListFind =(head,target)=>{
+ if(head===null) return false;
+ if(head.val==target) return true;
+ return linkedListFind(head.next,target)
+}
+
+console.log(linkedListFind(a,"C")) //A B C D
+
+
+
+
