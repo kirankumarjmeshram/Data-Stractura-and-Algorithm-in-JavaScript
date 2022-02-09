1377449d430b83a7f0117875489b7d143937bccc
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-02-09 05:57:40 +0530

    Create ProductOfArrayExceptItself.js

---
+++ goodProblems/ProductOfArrayExceptItself.js
@@ -0,0 +1,26 @@
+class Solution {
+    productExceptSelf(nums,n){
+      //code here
+      var leftNums = new Array(n);
+      var rightNums = new Array(n);
+      
+      leftNums[0] =1;
+      rightNums[n-1]=1;
+      
+      for(var i=1;i<n;i++){
+          leftNums[i]=nums[i-1]*leftNums[i-1]
+      }
+      
+       for(var i=n-2;i>=0;i--){
+          rightNums[i]=nums[i+1]*rightNums[i+1]
+      }
+      var newArray=[];
+        for(var i=0;i<n;i++){
+          newArray[i]=rightNums[i]*leftNums[i]
+      }
+       console.log(newArray)
+    }
+   
+}
+//console.log(productExceptSelf([10, 3, 5, 6, 2],5))
+
