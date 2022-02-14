ba2a31776dcb5e23b01ccea9de3246a145563a48
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-02-14 17:27:22 +0530

    closures

---
+++ AdvanceJavaScript/closure.js
@@ -0,0 +1,15 @@
+
+// A closure is the combination of a function bundled together (enclosed) with references to its
+//  surrounding state (the lexical environment). In other words, a closure gives you access to an 
+//  outer function’s scope from an inner function.
+//  In JavaScript, closures are created every time a function is created, at function creation time.
+
+
+function init() {
+    var name = 'Mozilla'; // name is a local variable created by init
+    function displayName() { // displayName() is the inner function, a closure
+      console.log(name); // use variable declared in the parent function
+    }
+    displayName();
+  }
+  init();
