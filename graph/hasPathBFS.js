cdbe4b5e1c4434a04c05b9c3cd487ae84a25366e
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-02-27 07:55:14 +0530

    has path by bfs

---
+++ graph/hasPathBFS.js
@@ -0,0 +1,42 @@
+const graph ={
+    f:['g','i'],
+    g:['h'],
+    h:[],
+    i:['g','k'],
+    j:["i"],
+    k:[],
+
+}
+
+const hasPath = (graph,src,dst)=>{
+    const queue=[src]
+    while(queue.length>0){
+        const current = queue.shift();
+        if(current===dst) return "Has Path";
+        
+        for(let neighbour of graph[current]){
+            queue.push(neighbour)
+        }
+
+    }
+    return "Has no Path"
+}
+
+console.log(hasPath({
+    f:['g','i'],
+    g:['h'],
+    h:[],
+    i:['g','k'],
+    j:["i"],
+    k:[],
+
+},"j","i"))// has Path
+console.log(hasPath({
+    f:['g','i'],
+    g:['h'],
+    h:[],
+    i:['g','k'],
+    j:["i"],
+    k:[],
+
+},"k","h"))// has no path
