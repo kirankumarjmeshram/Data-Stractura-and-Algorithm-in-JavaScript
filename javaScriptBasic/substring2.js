54f708524df2ba5afbed83c49dd9968988408715
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-04 16:01:48 +0530

    substring

---
+++ javaScriptBasic/substring2.js
@@ -0,0 +1,2 @@
+const str = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse unde quas sit tempore nesciunt cum necessitatibus id, perferendis debitis eius minima dicta tenetur corrupti, veritatis expedita voluptas velit hic."
+console.log(str.substring(0,10))//Lorem ipsu
