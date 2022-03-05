c9436dded9dfe8a2974bc61f1cc009481f4855d9
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-05 21:17:50 +0530

    sorting Object

--- sort/sortObject.js
+++ sort/sortObject.js
@@ -3,7 +3,15 @@ const users = [
     {name:'Jose', age:20},
     {name:'Aaron', age:40}
 ];
-const compareFuc = (item1,item2) => {
-  return item1.age - item2.age;
-};
-console.log(users.sort(compareFuc));
+// const compareFuc = (item1,item2) => {
+//   return item1.age - item2.age;
+// };
+// console.log(users.sort(compareFuc));
+
+console.log(users.sort((a,b)=>a.age-b.age))
+// [
+//     { name: 'Jose', age: 20 },
+//     { name: 'Luis', age: 25 },
+//     { name: 'Aaron', age: 40 }
+// ]
+  
