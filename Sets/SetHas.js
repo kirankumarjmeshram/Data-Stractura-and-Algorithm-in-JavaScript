af49462515e2b8192034c594be4b43123d683d4e
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-06 16:37:38 +0530

    get.has finding values in set

---
+++ Sets/SetHas.js
@@ -0,0 +1,9 @@
+let myArray = ['value1', 'value2', 'value3']
+
+// Use the regular Set constructor to transform an Array into a Set
+let mySet = new Set(myArray)
+
+mySet.has('value1')     // returns true
+
+// Use the spread operator to transform a set into an Array.
+console.log([...mySet]) // Will show you exactly the same Array as myArray
