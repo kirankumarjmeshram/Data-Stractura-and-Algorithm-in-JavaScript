809015553e3d9337211ce48dc68544694a1a3b0a
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-13 22:43:53 +0530

    Object.entities()

---
+++ Opps/ObjectEntities.js
@@ -0,0 +1,10 @@
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+
+  console.log(Object.entries(object1))//[ [ 'a', 'somestring' ], [ 'b', 42 ], [ 'c', false ] ]
+  console.log(object1)//{ a: 'somestring', b: 42, c: false }
+  
