0e2047f6ff9a6609b358b335772aa2a377b78c40
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-13 17:47:38 +0530

    at or charAt
    tells char at index

---
+++ String/string basic/at_CharAt.js
@@ -0,0 +1,8 @@
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+let index = 5;
+
+let chatAtindex = sentence.at(index);
+let chatAtindex2 = sentence.charAt(index);
+console.log(chatAtindex)//u
+console.log(chatAtindex2)//u
