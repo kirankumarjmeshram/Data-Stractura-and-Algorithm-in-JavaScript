d26724fbce8cececf11123c3931aa0ffb5a7be7e
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-13 17:52:41 +0530

    indexOf()
    The indexOf() method, given one argument: a substring to search for, searches the entire calling string, and returns the index of the first occurrence of the specified substring. Given a second argument: a number, the method returns the first occurrence of the specified substring at an index greater than or equal to the specified number.

---
+++ String/string basic/indexOf.js
@@ -0,0 +1,5 @@
+const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+const searchTerm = 'fox';
+
+console.log(paragraph.indexOf(searchTerm))//16
