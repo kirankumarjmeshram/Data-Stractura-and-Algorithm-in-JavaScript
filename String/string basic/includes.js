731e5f3e94288bf7edb759bfefd1cf4c7e748487
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-13 17:50:27 +0530

    includes()
    The includes() method performs a case-sensitive search to determine whether one string may be found within another string, returning true or false as appropriate.

---
+++ String/string basic/includes.js
@@ -0,0 +1,8 @@
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = "abw";
+const word2 ="quick"
+
+console.log(sentence.includes(word))//false
+console.log(sentence.includes(word2))//true
+
