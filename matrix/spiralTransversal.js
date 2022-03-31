6ad41ccbb6d67f7d3406e81d830df8d7c82e15ad
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-31 11:28:49 +0530

    spiral transversal

---
+++ matrix/spiralTransversal.js
@@ -0,0 +1,38 @@
+function spiralT(n,arr){
+    var ans=[];
+    var count=0;
+    var top=0;
+    var bottom =n-1;
+    var left = 0;
+    var right = n-1;
+    while(count<n*n){
+        
+        for(var i=left;i<=right;i++){
+            ans.push(arr[top][i]);
+            count++;
+        }
+        top++;
+        for(var i=top;i<=bottom;i++){
+            ans.push(arr[i][right]);
+            count++;
+        }
+        right--
+        
+        for(var i=right;i>=left;i--){
+            ans.push(arr[bottom][i]);
+            count++;
+        }
+        bottom--;
+        
+        for(var i=bottom;i>=top;i--){
+            ans.push(arr[i][left]);
+            count++;
+        }
+        left++
+        
+    }
+    
+    return ans.join(",")
+}
+
+console.log(spiralT(3, [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]]));
