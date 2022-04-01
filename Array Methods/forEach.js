253b79dbe70a8097d2a94787d494f6467d0aad44
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-04-01 18:09:21 +0530

    forEach

---
+++ Array Methods/forEach.js
@@ -0,0 +1,12 @@
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+  ];
+
+  let totalGrades =0;
+  students.forEach(el=>totalGrades+=el.grade)
+
+  console.log(totalGrades) //435
