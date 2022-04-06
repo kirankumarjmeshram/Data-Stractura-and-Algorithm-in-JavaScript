912c457611ef38656e3de9a114d4997dfe7d3997
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-04-06 10:27:58 +0530

    eval
    The eval() function evaluates the expression passed into it as a parameter and returns the result after evaluation.

---
+++ basicJS/eval.js
@@ -0,0 +1,9 @@
+// The eval() function evaluates the expression passed into it as a parameter and returns the result after evaluation.
+
+
+let x=10;
+let y=20;
+let ans1 = eval("x+y");
+let ans2 = eval(x+y);
+console.log(ans1);//30
+console.log(ans2);//30
