70d6e4bce248bc24369640c0e0a78619a50d42af
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-04-07 16:00:01 +0530

    eval()

--- basicJS/eval.js
+++ basicJS/eval.js
@@ -7,3 +7,16 @@ let ans1 = eval("x+y");
 let ans2 = eval(x+y);
 console.log(ans1);//30
 console.log(ans2);//30
+
+// const example = ({ a, b, c }) => {
+//     console.log(a, b, c);
+//    };
+//    example({a:0, b:1, c:2});
+
+
+let abc ={
+    a:0,
+    b:1,
+    c:2
+}
+
