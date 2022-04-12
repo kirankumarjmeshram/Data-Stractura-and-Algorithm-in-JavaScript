97df785454ca3b695dc529a87acc143f86a977e4
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-04-12 15:13:30 +0530

    merge two sorted linkedList

---
+++ linked List/mergeTwoSortedLL.js
@@ -0,0 +1,20 @@
+class LinkedList {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  function mergeLinkedLists(headOne, headTwo) {
+    if(headOne==null) return headTwo;
+      if(headTwo==null) return headOne;
+      
+      if(headOne.value<headTwo.value){
+          headOne.next= mergeLinkedLists(headOne.next,headTwo);
+          return headOne
+      }else{
+          headTwo.next = mergeLinkedLists(headOne,headTwo.next)
+          return headTwo
+      }
+      
+  }
