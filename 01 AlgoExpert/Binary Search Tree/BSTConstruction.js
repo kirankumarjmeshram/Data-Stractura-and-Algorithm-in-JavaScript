41ae6b8dd08adacb918b534a7627a8c735fbec7b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-06-03 16:16:11 +0530

    BST Construction

---
+++ 01 AlgoExpert/Binary Search Tree/BSTConstruction.js
@@ -0,0 +1,33 @@
+//https://www.algoexpert.io/questions/bst-construction
+
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  
+    insert(value) {
+      // Write your code here.
+      // Do not edit the return statement of this method.
+      return this;
+    }
+  
+    contains(value) {
+      // Write your code here.
+    }
+  
+    remove(value) {
+      // Write your code here.
+      // Do not edit the return statement of this method.
+      return this;
+    }
+  }
+  
+  // Do not edit the line below.
+ //exports.BST = BST;
+  
