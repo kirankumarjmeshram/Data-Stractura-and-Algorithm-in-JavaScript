b29359842a6a6fc86cb012e94472e0bab6271ac1
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-06-16 15:42:26 +0530

    dfs graph

---
+++ 01 AlgoExpert/Graphs/dfs.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      //here array = [];
+      console.log(array)
+      // Write your code here.
+      array.push(this.name);
+      for(const el of this.children){
+        el.depthFirstSearch(array)
+      }
+      return array
+          
+    }
+  }
