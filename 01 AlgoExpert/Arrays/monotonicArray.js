3694707eb98b3cc3c46d95f494ab80b0cebfd29c
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-09-27 15:32:47 +0530

    2nd method for monotonic array

--- 01 AlgoExpert/Arrays/monotonicArray.js
+++ 01 AlgoExpert/Arrays/monotonicArray.js
@@ -16,3 +16,31 @@ function isMonotonic(array) {
   
     return increasing || decreasing;
   }
+
+ // Second method
+
+ /*
+ function isMonotonic(array) {
+  // Write your code here.
+  if (array.length <= 2){
+    return true;
+  }
+  let direction = array[1] - array[0];
+  for(let i =2; i<array.length; i++){
+    if(direction === 0){
+      direction = array[i] - array[i-1];
+      continue;
+    }
+    if(breakDirection(direction, array[i-1], array[i])){
+      return false;
+    }
+  }
+  return true;
+}
+function breakDirection(direction, priviousInt, currentInt){
+  const difference = currentInt - priviousInt;
+  if(direction > 0) return difference <0;
+  return difference > 0;
+}
+ */
+
