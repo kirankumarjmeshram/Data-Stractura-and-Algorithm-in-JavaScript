9a29476da9c81f42b1bdb06df18018ef3260da42
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-09-28 20:25:23 +0530

    aarray of products solution 2

---
+++ 01 AlgoExpert/Arrays/arrarOfProducts2.js
@@ -0,0 +1,16 @@
+//solution 2
+
+function arrayOfProducts(array) {
+    // Write your code here.
+    const ans = [];
+    for(let i =0; i< array.length;i++){
+      let runningProduct = 1;
+      for(let j = 0; j < array.length;j++){
+        if(i!==j){
+              runningProduct *= array[j]
+        }
+        ans[i] = runningProduct
+      }
+    }
+    return ans;
+  }
