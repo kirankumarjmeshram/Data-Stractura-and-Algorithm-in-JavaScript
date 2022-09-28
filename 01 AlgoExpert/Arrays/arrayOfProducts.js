7922bc798d5def89dc15bd6e37418bfdf1a34073
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-09-28 20:24:12 +0530

    Array of products ans 1

---
+++ 01 AlgoExpert/Arrays/arrayOfProducts.js
@@ -0,0 +1,25 @@
+function arrayOfProducts(array) {
+    // Write your code here.
+      var n = array.length;
+      var leftProd =new Array(n);
+      var rightProd = new Array(n);
+      leftProd[0]=1;
+      rightProd[n-1]=1;
+      
+      for(var i=1;i<n;i++){
+          leftProd[i]=leftProd[i-1]*array[i-1];
+      }
+      
+      for(var j=n-2;j>=0;j--){
+          rightProd[j]=rightProd[j+1]*array[j+1];
+      }
+      
+      var ans=[];
+      
+      for(var k=0;k<n;k++){
+          ans.push(rightProd[k]*leftProd[k]);
+      }
+      
+      return ans;
+      
+  }
