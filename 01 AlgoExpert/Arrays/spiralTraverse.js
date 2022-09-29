d5b4c74e4eabd3c66c192346650cdac2e5561168
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-09-29 19:06:22 +0530

    updated trasversal

--- 01 AlgoExpert/Arrays/spiralTraverse.js
+++ 01 AlgoExpert/Arrays/spiralTraverse.js
@@ -1,25 +1,30 @@
 function spiralTraverse(array) {
-    // Write your code here.
-    const result = [];
-    let top = 0;
-    let bottom = array.length-1;
-    let left = 0;
-    let right = array[0].length-1;
-  
-    while(top  <= bottom && left <= right){
-      for(let i = left; i <= right;i++){
-        result.push(array[top][i]);
-      }
-      top++;
-      for(let i = top; i <= bottom;i++){
-        result.push(array[i][right]);
-      }
-      right--;
-      for(let i = right; i>=left;i--){
-        result.push(array[bottom][i])
-      }
-      bottom--;
+  // Write your code here.
+  const result = [];
+  let top = 0;
+  let bottom = array.length-1;
+  let left = 0;
+  let right = array[0].length-1;
+
+  while(top  <= bottom && left <= right){
+    for(let i = left; i <= right;i++){
+      result.push(array[top][i]);
+    }
+    top++;
+    for(let i = top; i <= bottom;i++){
+      result.push(array[i][right]);
+    }
+    right--;
+    for(let i = right; i>=left;i--){
+
+      result.push(array[bottom][i]);
+
+    }
+    bottom--;
       for(let i = bottom; i>=top;i--){
+        if(left === right){
+          break;
+        }
         result.push(array[i][left])
       }
       left++
@@ -31,7 +36,6 @@ function spiralTraverse(array) {
   
   console.log(spiralTraverse([
     [1, 2, 3, 4],
-    [12, 13, 14, 5],
-    [11, 16, 15, 6],
-    [10, 9, 8, 7]
+    [10, 11, 12, 5],
+    [9, 8, 7, 6]
   ]))
