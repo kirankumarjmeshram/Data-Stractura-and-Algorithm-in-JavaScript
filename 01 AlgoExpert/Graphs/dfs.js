be87893691962d331183da3c74f59d007fb4f1f5
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-04 21:02:57 +0530

    bfs

--- 01 AlgoExpert/Graphs/dfs.js
+++ 01 AlgoExpert/Graphs/dfs.js
@@ -1,29 +1,25 @@
-// Do not edit the class below except
-// for the breadthFirstSearch method.
-// Feel free to add new properties
-// and methods to the class.
 class Node {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
-  }
-
-  addChild(name) {
-    this.children.push(new Node(name));
-    return this;
-  }
-
-  breadthFirstSearch(array) {
-    // Write your code here.
-    // here array = [];
-    const queue = [this];
-    while(queue.length > 0){
-      const current = queue.shift();
-      array.push(current.name);
-      for(const child of current.children){
-        queue.push(child);
-      }
+    constructor(name) {
+      this.name = name;
+      this.children = [];
     }
-    return array;
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      //here array = [];
+      console.log(array)
+      // Write your code here.
+      array.push(this.name);
+      for(const el of this.children){
+        el.depthFirstSearch(array)
+      }
+      return array
+          
+    }
   }
-}
+  
+ 
