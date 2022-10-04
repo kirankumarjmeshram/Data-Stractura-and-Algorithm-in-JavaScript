f786513abf02f73d025c22ff529e481ab2051f17
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-04 21:08:04 +0530

    Update dfs.js

--- 01 AlgoExpert/Graphs/dfs.js
+++ 01 AlgoExpert/Graphs/dfs.js
@@ -21,5 +21,18 @@ class Node {
           
     }
   }
+
+  // breadthFirstSearch(array) {
+  //   // Write your code here.
+  //   const queue = [this];
+  //   while(queue.length > 0){
+  //     const current = queue.shift();
+  //     array.push(current.name);
+  //     for(const child of current.children){
+  //       queue.push(child);
+  //     }
+  //   }
+  //   return array;
+  // }
   
  
