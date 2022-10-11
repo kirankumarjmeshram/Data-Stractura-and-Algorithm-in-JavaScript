9901f623059afd718b41ee73c0a8f6f083b5ab56
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-11 09:22:46 +0530

    RiverSizes

--- 01 AlgoExpert/Graphs/riverSize.js
+++ 01 AlgoExpert/Graphs/riverSize.js
@@ -1,4 +1,44 @@
-let matrix = [[1,1,0],[0,1,0],[0,0,1]];
-let visited = matrix.map(el => el.map(value => false));
-console.log(matrix)
-console.log(visited)
+function riverSizes(matrix) {
+    // Write your code here.
+    const sizes = [];
+    const visited = matrix.map(row => row.map(value => false));
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if(visited[i][j]) continue;
+        traverseNode(i, j, matrix, visited, sizes);
+      }
+    }
+    return sizes;
+  }
+  function traverseNode(i, j, matrix, visited, sizes) {
+    let currentRiverSize = 0;
+    const nodesToExplore = [[i, j]];
+    while (nodesToExplore.length){
+      const currentNode = nodesToExplore.pop();
+      i = currentNode[0];
+      j = currentNode[1];
+      if(visited[i][j]) continue;
+      visited[i][j] = true;
+      if(matrix[i][j] == 0) continue;
+      currentRiverSize++;
+      const unvisitedNeighbours = getUnvisitedNeighbours(i, j, matrix, visited);
+      for(const neighbour of unvisitedNeighbours){
+        nodesToExplore.push(neighbour);
+      }
+    }
+    if(currentRiverSize > 0) sizes.push(currentRiverSize)
+  }
+  
+  function getUnvisitedNeighbours(i, j, matrix, visited) {
+    const unvisitedNeighbours = [];
+    if (i > 0 && !visited[i-1][j]) {
+      unvisitedNeighbours.push([i-1, j]);
+    }if(i < matrix.length -1 && !visited[i+1][j]){
+      unvisitedNeighbours.push([i+1, j]);
+    }if(j > 0 && !visited[i][j-1]){
+      unvisitedNeighbours.push([i, j-1]);
+    }if(j < matrix[0].length - 1 && !visited[i][j+1]){
+      unvisitedNeighbours.push([i, j+1]);
+    }
+    return unvisitedNeighbours;
+  }
