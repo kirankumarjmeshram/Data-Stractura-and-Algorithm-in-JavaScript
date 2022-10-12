d6b9993121703b34e9830d76646709c007c25003
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-12 21:10:51 +0530

    longestPeak

---
+++ 01 AlgoExpert/Arrays/longestPeak.js
@@ -0,0 +1,24 @@
+function longestPeak(array) {
+    // Write your code here.
+    let longestPeakLengh = 0;
+    let i =1;
+    while(i < array.length - 1){
+      const isPeak = array[i-1] < array[i] && array[i+1] < array[i];
+      if(!isPeak){
+        i++;
+        continue;
+      }
+      let leftIdx = i-2;
+      while(leftIdx >=0 && array[leftIdx] < array[leftIdx+1]){
+        leftIdx--;
+      }
+      let rightIdx = i+2;
+      while (rightIdx < array.length && array[rightIdx] < array[rightIdx-1]) {
+        rightIdx++;
+      }
+      const currentPeakLength = rightIdx - leftIdx -1;
+      longestPeakLengh = Math.max(longestPeakLengh, currentPeakLength);
+      i = rightIdx;
+    }
+    return longestPeakLengh;
+  }
