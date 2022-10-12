da77d9752982ec4b3ed17dbe46057c10357f9eb6
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-12 15:56:07 +0530

    Update Spiral Traversal

--- 01 AlgoExpert/Arrays/spiralTraverse.js
+++ 01 AlgoExpert/Arrays/spiralTraverse.js
@@ -10,32 +10,27 @@ function spiralTraverse(array) {
     for(let i = left; i <= right;i++){
       result.push(array[top][i]);
     }
-    top++;
-    for(let i = top; i <= bottom;i++){
+    
+    for(let i = top+1; i <= bottom;i++){
       result.push(array[i][right]);
     }
-    right--;
-    for(let i = right; i>=left;i--){
-
-      result.push(array[bottom][i]);
-
+    
+    for(let i = right-1; i>=left;i--){
+     if (top === bottom) break;
+       result.push(array[bottom][i])
     }
-    bottom--;
-      for(let i = bottom; i>=top;i--){
-        if(left === right){
-          break;
-        }
-        result.push(array[i][left])
+    
+    for(let i = bottom-1; i>top;i--){
+        if(left === right) break;
+        result.push(array[i][left]);
       }
+
+      top++;
+      right--;
+      bottom--;
       left++
       
     }
     return result;
     
   }
-  
-  console.log(spiralTraverse([
-    [1, 2, 3, 4],
-    [10, 11, 12, 5],
-    [9, 8, 7, 6]
-  ]))
