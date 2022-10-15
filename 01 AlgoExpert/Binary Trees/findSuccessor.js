190c76cff0e51afc7408fe5142d851b5ece20cba
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-15 16:32:33 +0530

    find Succosor

---
+++ 01 AlgoExpert/Binary Trees/findSuccessor.js
@@ -0,0 +1,27 @@
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+      this.parent = null;
+    }
+  }
+  
+  function findSuccessor(tree, node) {
+    // Write your code here.
+    const inOrderTraversalOrder = getInOrderTraversalOrder(tree);
+    for (let idx = 0; idx < inOrderTraversalOrder.length; idx++) {
+      const currentNode = inOrderTraversalOrder[idx];
+      if(currentNode !== node) continue;
+      if(idx === inOrderTraversalOrder.length-1) return null;
+      return inOrderTraversalOrder[idx + 1];
+    }
+  }
+  function getInOrderTraversalOrder(node, order = []){
+    if(node === null) return order;
+    getInOrderTraversalOrder(node.left, order);
+    order.push(node)
+    getInOrderTraversalOrder(node.right, order);
+    return order;
+  }
+  
