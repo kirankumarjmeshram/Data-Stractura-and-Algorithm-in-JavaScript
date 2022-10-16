869c89aa39fc6c2067e7a77b79ad3eb72aceddad
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-16 14:08:41 +0530

    reconstruct Binary Search Tree

---
+++ 01 AlgoExpert/Binary Search Tree/reconstructBST.js
@@ -0,0 +1,28 @@
+class BST {
+    constructor(value, left = null, right = null) {
+      this.value = value;
+      this.left = left;
+      this.right = right;
+    }
+  }
+  
+  function reconstructBst(preOrderTraversalValues) {
+    // Write your code here.
+    if(preOrderTraversalValues.length === 0) return null;
+    const currentValue = preOrderTraversalValues[0];
+    let rightSubTreeRootIdx = preOrderTraversalValues.length;
+  
+    for (let idx = 1; idx < preOrderTraversalValues.length; idx++) {
+      const value = preOrderTraversalValues[idx];
+      if(value >= currentValue){
+        rightSubTreeRootIdx = idx;
+        break;
+      }
+    }
+    const leftSubTree = reconstructBst(preOrderTraversalValues.slice(1,rightSubTreeRootIdx));
+    const rightSubTree = reconstructBst(preOrderTraversalValues.slice(rightSubTreeRootIdx));
+    return new BST(currentValue, leftSubTree, rightSubTree)
+    
+    
+  }
+  
