d16a177718f22fc031226274a65b2d8d3676c56c
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-10-18 12:18:04 +0530

    shiftedBinarySearch

---
+++ 01 AlgoExpert/Searching/shiftedBinarySearch.js
@@ -0,0 +1,23 @@
+function shiftedBinarySearch(array, target) {
+    let low = 0;
+      let high = array.length-1;
+      while(low <= high){
+          let mid = Math.floor(low + (high-low)/2);
+          if(target === array[mid]){
+              return mid;
+          }else if(array[low] < array[mid] ){
+             if(target < array[mid] && target >= array[low]){
+              high = mid-1
+             }else{
+              low = mid+1
+             }
+          }else{
+              if(target > array[mid] && target <= array[high]){
+                  low = mid+1;
+              }else{
+                  high= mid -1;
+              }
+          }
+      }
+      return -1
+  }
