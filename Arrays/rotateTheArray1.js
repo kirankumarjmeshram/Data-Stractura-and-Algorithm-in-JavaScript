a1b0e6785e568068698ba8206870293f19e36c45
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-12 19:01:05 +0530

    rotate array without using extra space

--- Arrays/rotateTheArray1.js
+++ Arrays/rotateTheArray1.js
@@ -14,3 +14,20 @@ function rotateArray(arr,n,k){
 }
 console.log(rotateArray([1, 2, 3, 4, 5],5,2))
 
+
+// var rotate = function(nums, k) {
+//     k = k%nums.length
+//     let reverse = function(i, j){
+//     while(i < j){
+//         let temp = nums[i]
+//         nums[i] = nums[j]
+//         nums[j] = temp
+//         i++
+//         j--
+//     }
+//    } 
+// 	reverse(0, nums.length-1); 
+// 	 reverse(0, k-1)
+//    reverse(k, nums.length-1)
+// };
+
