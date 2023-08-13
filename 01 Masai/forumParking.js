65a0123ea12c8d42bfe73ca131ddbec3e2d5bf55
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-13 17:34:04 +0530

    Create forumParking.js

---
+++ 01 Masai/forumParking.js
@@ -0,0 +1,21 @@
+function forumParking(k, arrivalT, departureT){
+    let n = arrivalT.length;
+    let count = 1;
+    for(let i=0;i<n;i++){
+        if(arrivalT[i+1]<departureT[i]){
+            count++;
+        }
+    }
+    if(count==k){
+        return "Possible"
+    }else{
+        return "Not Possible"
+    }
+}
+
+console.log(forumParking(1,[1,3,5], [2,6,8]))
+
+check for this case 
+// 4
+// 4 3 6 7 11
+// 9 4 10 9 14
