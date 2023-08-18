c8c55deef4e97905544132b96c09faabfc4ff5d3
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-08-18 19:12:31 +0530

    solution O((1) space using transpose

--- matrix/90degreeClockwise.js
+++ matrix/90degreeClockwise.js
@@ -23,3 +23,26 @@ rotateBy90Clockwise(4,[ [ 5, 1, 5, 1 ],
                                     // 4 3 2 1
                                     // 8 7 6 5
                                     // 4 3 2 1
+
+                                    var rotate = function(matrix) {
+                                        let n = matrix.length;
+                                    // transpose : rotate wrt diagonal
+                                        for(let i=0;i<n;i++){
+                                            for(let j=0;j<i;j++){
+                                                let temp;
+                                                temp = matrix[i][j];
+                                                matrix[i][j] = matrix[j][i];
+                                                matrix[j][i] = temp;
+                                            }
+                                        }
+                                    // rotate wrt middle column
+                                        for(let i=0;i<n;i++){
+                                            for(let j =0;j<Math.floor(n/2);j++){
+                                                let temp;
+                                                temp = matrix[i][j];
+                                                matrix[i][j] = matrix[i][n-1-j];
+                                                matrix[i][n-1-j] = temp;
+                                            }
+                                        }
+                                        return matrix;
+                                    };
