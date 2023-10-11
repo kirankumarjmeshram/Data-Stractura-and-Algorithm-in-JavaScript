54b22ee6b7d95120f337915019be803fccf96da9
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-11 09:48:52 +0530

    Introduction to Currying and Partial Application

---
+++ freeCodeCamp/JavaScript Algorithms and Data Structures/Functional Programming/Curring.js
@@ -0,0 +1,27 @@
+function add(x) {
+    // Only change code below this line
+    return function(y){
+      return function(z){
+        return x+y+z
+      }
+    }
+  
+    // Only change code above this line
+  }
+  
+  console.log(add(10)(20)(30));//60
+
+
+  function unCurried(x, y) {
+    return x + y;
+  }
+  
+  function curried(x) {
+    return function(y) {
+      return x + y;
+    }
+  }
+  
+  const curried = x => y => x + y
+  
+  curried(1)(2)//3
