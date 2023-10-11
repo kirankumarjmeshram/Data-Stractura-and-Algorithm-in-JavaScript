173b2cc59d40ae28a9232ef47018e35c5861ba16
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-11 09:46:26 +0530

    check some positive

---
+++ freeCodeCamp/JavaScript Algorithms and Data Structures/Functional Programming/checkPositiveSome.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+    // Only change code below this line
+    return arr.some((el)=>el>0)
+  
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
