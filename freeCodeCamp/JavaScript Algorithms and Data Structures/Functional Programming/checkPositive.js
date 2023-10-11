dec06ad240bd87bc958adbb8fe209b1c340ca85c
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-11 09:45:01 +0530

    Use the every Method to Check that Every Element in an Array Meets a Criteria

---
+++ freeCodeCamp/JavaScript Algorithms and Data Structures/Functional Programming/checkPositive.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+    // Only change code below this line
+  return arr.every((el)=>el>0)
+  
+    // Only change code above this line
+  }
+  
+  console.log(checkPositive([1, 2, 3, -4, 5]));//false
