2b2f12cdbd4032ae2512546b2eb632c897fd23c9
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-12 14:53:32 +0530

    Sum All Primes

---
+++ freeCodeCamp/JS DSA/SumAllPrimes.js
@@ -0,0 +1,23 @@
+function sumPrimes(num) {
+    function isPrime(n) {
+      const sqrt = Math.sqrt(n);
+      if(n<2) return false;
+      for (let i = 2; i <= sqrt; i++) {
+        if (n % i === 0)
+          return false;
+      }
+      return true;
+    }
+  
+    let sum = 0;
+  
+    for(let i=0;i<num;i++){
+      if(isPrime(i)){
+        sum += i;
+      }
+    }
+    
+    return sum;
+  }
+  
+  console.log(sumPrimes(10));
