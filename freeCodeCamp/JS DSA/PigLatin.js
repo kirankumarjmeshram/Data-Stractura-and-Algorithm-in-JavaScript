3fc44609c56493c7d930d9c290ec16a71719ff33
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-16 21:10:40 +0530

    diff ways

--- freeCodeCamp/JS DSA/PigLatin.js
+++ freeCodeCamp/JS DSA/PigLatin.js
@@ -19,3 +19,61 @@ function translatePigLatin(str) {
   }
   
   console.log(translatePigLatin("consonant")); // onsonantcay
+
+  /**
+   * function translatePigLatin(str) {
+  // Create variables to be used
+  var pigLatin = "";
+  var regex = /[aeiou]/gi;
+
+  // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + "way";
+  } else if (str.match(regex) === null) {
+    // Check if the string contains only consonants
+    pigLatin = str + "ay";
+  } else {
+    // Find how many consonants before the first vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+  }
+
+  return pigLatin;
+}
+
+// test here
+translatePigLatin("consonant");
+
+
+function translatePigLatin(str) {
+  if (str.match(/^[aeiou]/)) return str + "way";
+
+  const consonantCluster = str.match(/^[^aeiou]+/)[0];
+  return str.substring(consonantCluster.length) + consonantCluster + "ay";
+}
+
+// test here
+translatePigLatin("consonant");
+
+
+   */
+
+// function translatePigLatin(str) {
+//     return str
+//       .replace(/^[aeiou]\w*/, "$&way")
+//       .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+//   }
+  
+//   // test here
+//   translatePigLatin("consonant");
+
+// function translatePigLatin(str, charPos = 0) {
+//     return ['a', 'e', 'i', 'o', 'u'].includes(str[0])
+//       ? str + (charPos === 0 ? 'way' : 'ay')
+//       : charPos === str.length
+//         ? str + 'ay'
+//         : translatePigLatin(str.slice(1) + str[0], charPos + 1);
+//   }
