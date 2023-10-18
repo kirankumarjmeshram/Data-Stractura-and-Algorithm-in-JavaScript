ca4739bbf19000e2a4892212f8f0c6d1df7284cc
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-19 01:16:31 +0530

    Update numberOfWaysToMakeChanges.js

--- 01 AlgoExpert/Dynamic Programming/numberOfWaysToMakeChanges.js
+++ 01 AlgoExpert/Dynamic Programming/numberOfWaysToMakeChanges.js
@@ -2,9 +2,9 @@ function numberOfWaysToMakeChanges (n, denoms){
     const ways = new Array(n+1).fill(0);
     ways[0] =1;
     for(let denom of denoms) {
-        for(let i =1; i < n+1; i++){
-            if(denom <= i){
-                ways[i] += ways[i-denom]
+        for(let amount =1; amount < n+1; amount++){
+            if(denom <= amount){
+                ways[amount] += ways[amount-denom]
             }
         }
     }
