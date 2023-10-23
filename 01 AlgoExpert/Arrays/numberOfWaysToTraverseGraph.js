412fb2009d11da02920e2bf96ca00b574b5bd77e
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-23 23:54:26 +0530

    numberOfWaysToTraverseGraph

---
+++ 01 AlgoExpert/Arrays/numberOfWaysToTraverseGraph.js
@@ -0,0 +1,23 @@
+function numberOfWaysToTraverseGraph(width, height) {
+    // Write your code here.
+    const numberOfWays = [];
+    for(let i=0;i<height+1;i++){
+      numberOfWays.push([]);
+      for(let j=0;j<width+1;j++){
+        numberOfWays[i].push(0)
+      }
+    }
+    for(let widthIdx = 1; widthIdx<width+1;widthIdx++){
+      for(let heightIdx=1;heightIdx<height+1;heightIdx++){
+        if(widthIdx === 1|| heightIdx===1){
+          numberOfWays[heightIdx][widthIdx] =1;
+        }else{
+          const waysLeft = numberOfWays[heightIdx][widthIdx-1];
+          const waysUp = numberOfWays[heightIdx-1][widthIdx];
+          numberOfWays[heightIdx][widthIdx] = waysLeft+waysUp
+        }
+      }
+    }
+    return numberOfWays[height][width];
+  }
+  
