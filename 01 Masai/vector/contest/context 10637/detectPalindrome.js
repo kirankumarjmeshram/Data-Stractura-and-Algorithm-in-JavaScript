77429a3baf060ff989a39f1119770193377432ec
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-12-01 16:47:20 +0530

    Detect Palindrome

---
+++ 01 Masai/vector/contest/context 10637/detectPalindrome.js
@@ -0,0 +1,10 @@
+function detectPalindrome(num){
+    num = num.toString()
+    let n = num.length
+    for(let i=0;i<Math.floor(n/2);i++){
+        if(num[i]!==num[n-1-i]){
+            return console.log("No")
+        }
+    }
+    return console.log("Yes")
+}
