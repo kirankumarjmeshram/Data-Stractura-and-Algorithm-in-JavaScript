2dd1e59788afc6b95b4ecfd6f7899e283196dfc1
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-12-03 18:27:48 +0530

    day of the week

---
+++ 01 vector/contest/context Day4 assignment 4 2687/dayOfTheWeek.js
@@ -0,0 +1,19 @@
+function dayOfTheWeek(n){
+    let day;
+    switch(n){
+        case 0:
+            return "Sunday" ;
+        case 1:
+            return  "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5: 
+            return "Friday";
+        case 6:
+            return "Saturday";
+    }
+}
