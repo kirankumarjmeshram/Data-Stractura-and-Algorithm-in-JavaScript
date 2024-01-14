c4ad63d4a243c09895b7c3c78dd87d8d8f88b34a
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-01-14 23:11:08 +0530

    permutations

---
+++ 01 vector/contest/context Day 9/permutations.js
@@ -0,0 +1,25 @@
+function generatePermutation(n, arr){
+    let permutations = [];
+    permutationHelper(0,n,arr,permutations);
+    // return permutations;
+    for(let i=0;i<permutations.length;i++){
+        console.log(permutations[i].join(" "));
+    }
+}
+
+function permutationHelper (index, n, arr, permutations){
+    if(index === n-1){
+        permutations.push(arr.slice());
+    }
+    for(let i=index;i<n;i++){
+        swap(arr, i, index);
+        permutationHelper(index+1, n, arr, permutations);
+        swap(arr, i, index);
+    }
+}
+function swap(arr, i, j){
+    let temp;
+    temp = arr[i];
+    arr[i]=arr[j];
+    arr[j] = temp;
+}
