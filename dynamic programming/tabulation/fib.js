aa1ddf02653a8967634ec1cca26478e656e6c5c6
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-03-11 23:10:29 +0530

    fib using tabulation

---
+++ dynamic programming/tabulation/fib.js
@@ -0,0 +1,11 @@
+function fib(n){
+    let table = new Array(n+1).fill(0);
+    table[1] = 1;
+    for(let i=1;i<=n;i++){
+        table[i+1]+=table[i];
+        table[i+2]+=table[i]
+    }
+    return table[n];
+}
+
+console.log(fib(5));
