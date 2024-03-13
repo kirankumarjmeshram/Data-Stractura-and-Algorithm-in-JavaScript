1b412f14ba3c41480f2ab4740c1f547239719a51
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-03-13 08:50:25 +0530

    canSum tabulation

---
+++ dynamic programming/tabulation/canSum.js
@@ -0,0 +1,18 @@
+function canSum(targetSum, numbers){
+    let table = new Array(targetSum+1).fill(false);
+    table[0] = true;
+    for(let num of numbers){
+        table[num] = true;
+    }
+    for(let i=1;i<=targetSum;i++){
+        if(table[i]=== true){
+            for(let num of numbers){
+                table[i+num] = true;
+            }
+        }
+    }
+    return table[targetSum];
+}
+
+console.log(canSum(7,[2,4,4]))//false
+console.log(canSum(7,[1,4,4]))// true
