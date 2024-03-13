9aec02a083d82b809e24f5bfafd8459b5309a3a0
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-03-13 23:37:52 +0530

    how sum recursive

---
+++ dynamic programming/recursion/howSum.js
@@ -0,0 +1,16 @@
+function howSum(targetSum, numbers){
+    if(targetSum === 0) return [];
+    if(targetSum < 0) return null;
+    for(let num of numbers){
+        let remainder = targetSum - num;
+        // if(howSum(remainder,numbers)!== null){
+        //     return [...howSum(remainder,numbers), num];
+        // }
+        let remainderResult = howSum(remainder, numbers);
+        if(remainderResult !== null){
+            return [...remainderResult, num];
+        }
+    }
+    return null
+}
+console.log(howSum(50,[2,6,3]))
