40152dd948ab27e6bfd4c6288113b8ed2f214cd4
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-04-01 06:27:14 +0530

    Queue

---
+++ Queues/Queue.js
@@ -0,0 +1,30 @@
+function Queue () { 
+    let collection = [];
+    this.print = function() {
+        console.log(collection);
+    };
+    this.enqueue = function(element) {
+        collection.push(element);
+    };
+    this.dequeue = function() {
+        return collection.shift(); 
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
+
+let q = new Queue(); 
+q.enqueue('a'); 
+q.enqueue('b');
+q.enqueue('c');
+q.print();
+q.dequeue();
+console.log(q.front());
+q.print();
