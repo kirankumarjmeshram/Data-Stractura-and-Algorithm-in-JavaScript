f85074d30e75e3a5f23cf6b66e19f51970dace5c
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-04-03 13:02:52 +0530

    Update courseSchedule.js using using object

--- 01 vector/contest/DSA Leetcode Assignment 3/courseSchedule.js
+++ 01 vector/contest/DSA Leetcode Assignment 3/courseSchedule.js
@@ -28,3 +28,42 @@ var canFinish = function(numCourses, prerequisites) {
     return true;
 
 };
+
+// using object 
+var canFinish2 = function(numCourses, prerequisites) {
+    let preMap = {};
+    
+    // Initialize the object with empty arrays for each course
+    for (let i = 0; i < numCourses; i++) {
+        preMap[i] = [];
+    }
+
+    // Populate the object with prerequisites
+    for(let [crs, pre] of prerequisites) {
+        if (!preMap[crs]) {
+            preMap[crs] = [];
+        }
+        preMap[crs].push(pre);
+    }
+
+    let visitSet = new Set();
+
+    function dfs(crs) {
+        if(visitSet.has(crs)) return false;
+        if (!preMap[crs].length) return true;
+
+        visitSet.add(crs);
+        for(let pre of preMap[crs]) {
+            if(!dfs(pre)) return false;
+        }
+        visitSet.delete(crs);
+        preMap[crs] = [];
+        return true;
+    }
+
+    for (let crs = 0; crs < numCourses; crs++) {
+        if(!dfs(crs)) return false;
+    }
+
+    return true;
+};
