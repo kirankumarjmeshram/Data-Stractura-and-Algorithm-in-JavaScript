2ece49cc608df72b2e189622c87a70413537907b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-05-20 23:46:16 +0530

    longest increasing subsequence

---
+++ 01 leetcode/Top Interview 150/1D DP/ongest-increasing-subsequence.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/longest-increasing-subsequenc
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    //nums => [10,9,2,5,3,7,101,18]
+    // dp  => [1, 1,1,2,2,3, 4, 1]
+    let n = nums.length;
+    let dp = new Array(n).fill(1);
+    for(let i=1;i<n;i++){
+        for(let j=0;j<i;j++){
+            if(nums[i]>nums[j]){
+                dp[i] = Math.max(dp[i], 1+dp[j])
+            }
+        }
+    }
+    return Math.max(...dp)
+
+};
