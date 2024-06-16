ac739998ab75b05752095a2e1898ab0047446f30
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-06-16 22:29:15 +0530

    Add two numbers

---
+++ 01 leetcode/Top Interview 150/Linked List/AddTwoNumbers.js
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/add-two-numbers/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode();
+    let curr = dummy;
+    let carry = 0, total = 0;
+    while(l1 || l2 || carry){
+        total = carry
+        if(l1){
+            total +=l1.val;
+            l1 = l1.next;
+        }
+        if(l2){
+            total += l2.val;
+            l2 = l2.next;
+        }
+        let num = total%10;
+        carry = Math.floor(total/10);
+        dummy.next = new ListNode(num);
+        dummy = dummy.next;
+    }
+    return curr.next
+};
