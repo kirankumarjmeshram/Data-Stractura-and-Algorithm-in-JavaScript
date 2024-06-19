dbae5653ea8ff7dd3092dd0700c3af3b479c6751
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-06-19 09:44:37 +0530

    canCompleteCircuit

---
+++ 01 AlgoExpert/Strings/canCompleteCircuit.js
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/gas-station/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    let n = gas.length;
+    let totalGas = 0;
+    let currGas = 0;
+    let startingIdx = 0;
+    for(let i=0;i<n;i++){
+        totalGas+= gas[i] - cost[i];
+        currGas += gas[i]-cost[i];
+        if(currGas<0){
+            currGas = 0;
+            startingIdx =i+1
+       }
+    }
+    return totalGas>=0?startingIdx:-1;
+};
