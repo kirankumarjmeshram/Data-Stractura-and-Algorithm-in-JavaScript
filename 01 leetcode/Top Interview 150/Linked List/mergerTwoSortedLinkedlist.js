783737f345c4c1709c1d89dde49dacca7cb41d22
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-06-23 21:58:26 +0530

    Create mergerTwoSortedLinkedlist.js

---
+++ 01 leetcode/Top Interview 150/Linked List/mergerTwoSortedLinkedlist.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/?envType=study-plan-v2&envId=top-interview-150
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode();
+    let curr = dummy;
+
+    while(list1 !== null && list2 !== null){
+        if(list1.val < list2.val){
+            curr.next = list1
+            list1 = list1.next;
+        }else{
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next
+    }
+
+    curr.next = list1 || list2;
+    return dummy.next;
+};
