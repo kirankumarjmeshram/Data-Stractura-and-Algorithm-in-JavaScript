179d04dc71e50c042c2f6186ba409fe071a149fb
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-06-27 10:07:20 +0530

    merge k sorted linkedlidt

---
+++ 01 leetcode/LinkedList/mergeKSortedLinkedList.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/merge-k-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+ 
+var mergeKLists = function(lists) {
+    let ans = null;
+    for(let i = 0 ; i < lists.length ; i++) {
+        ans = mergeTwoLists(ans,lists[i])
+    }
+    return ans;
+};
+
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode();
+    let curr = dummy;
+
+    while(list1 !== null && list2 !== null){
+        if(list1.val < list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        }else{
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next;
+    }
+    if(!list1){
+        curr.next = list2
+    }else{
+        curr.next = list1
+    }
+
+    return dummy.next;
+};
