e42b143bcedc12fee8541f68c4c4c6c296d3aa64
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-08-02 23:03:32 +0530

    Update bstTraversal.js

--- 01 AlgoExpert/Binary Search Tree/bstTraversal.js
+++ 01 AlgoExpert/Binary Search Tree/bstTraversal.js
@@ -1,34 +1,34 @@
 function inOrderTraverse(tree, array) {
-    // Write your code here.
-    if(tree !== null){
-        inOrderTraverse(tree.left, array);
-        array.push(tree.value);
-        inOrderTraverse(tree.right, array)
-    }
-    return array
-    
+  // Write your code here.
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array)
   }
-  
-  function preOrderTraverse(tree, array) {
-    // Write your code here.
-    if(tree !== null){
-        array.push(tree.value);
-        preOrderTraverse(tree.left, array);
-        preOrderTraverse(tree.right,array)
-    }
-    return array
+  return array
+
+}
+
+function preOrderTraverse(tree, array) {
+  // Write your code here.
+  if (tree !== null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array)
   }
-  
-  function postOrderTraverse(tree, array) {
-    // Write your code here.
-    if(tree !== null){
-       
-        postOrderTraverse(tree.left, array);
-        postOrderTraverse(tree.right,array);
-        array.push(tree.value);
-    }
-    return array
+  return array
+}
+
+function postOrderTraverse(tree, array) {
+  // Write your code here.
+  if (tree !== null) {
+
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
   }
+  return array
+}
 
 
 
