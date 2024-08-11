dc6e43b6c8d23d37bca8a25e310a71cbe5d94190
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-08-11 10:28:07 +0530

    selectionSortRecursive

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -16,3 +16,29 @@ var sortArray = function(nums) {
     return nums;
 };
 
+// selectionSortRecursive
+function selectionSortRecursive(N,arr){
+    //write code here
+    selectionSort(arr, 0, N);
+    return console.log(arr.join(" "));
+}
+
+function selectionSort(arr, start, N) {
+    if( start >=N-1) {
+        return;
+    }
+    let minIndex = start;
+    for(let i=start+1;i<N;i++) {
+        if(arr[i] < arr[minIndex]){
+            minIndex = i;
+        }
+    }
+    if(minIndex !== start) {
+        let temp = arr[start];
+        arr[start] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    
+    selectionSort(arr, start + 1, N);
+}
+
