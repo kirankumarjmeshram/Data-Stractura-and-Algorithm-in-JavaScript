dc55b67e11f225e799513b304ec560c4b54681ec
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-10-27 17:26:07 +0530

    using binary search

--- 01 leetcode/Binary Search/HIndex1.js
+++ 01 leetcode/Binary Search/HIndex1.js
@@ -1,6 +1,22 @@
 // https://leetcode.com/problems/h-index/description/
 
-
+var hIndex_sol = function(citations) {
+    citations = citations.sort((a,b)=> a-b);
+    let n = citations.length;
+    let low =0;
+    let high = n-1;
+    let hindex = 0;
+    while(low <= high) {
+        let mid = Math.floor((high-low)/2 +low);
+        if(citations[mid] >= n-mid){
+            high = mid - 1;
+            hindex = n-mid
+        }else{
+            low = mid+1
+        }
+    }
+    return hindex;
+};
 
 
 
