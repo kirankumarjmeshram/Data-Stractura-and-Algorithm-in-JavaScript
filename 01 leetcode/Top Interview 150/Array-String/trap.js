fc5b6c16c0f0fb0a656a041e302170b0eb8fe6e2
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-11-30 18:32:20 +0530

    two pointer space optimised opproached

--- 01 leetcode/Top Interview 150/Array-String/trap.js
+++ 01 leetcode/Top Interview 150/Array-String/trap.js
@@ -26,3 +26,29 @@ var trap = function(height) {
     }
     return water;
 };
+
+// Space optimised solution
+//two pointer approach
+
+var trapSol2 = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = height[left];
+    let rightMax = height[right];
+    let water = 0;
+
+    while (left < right) {
+        if(leftMax < rightMax) {
+            left++;
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left]
+        } else {
+            right--;
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right]
+        }
+    }
+
+    return water;
+
+}
