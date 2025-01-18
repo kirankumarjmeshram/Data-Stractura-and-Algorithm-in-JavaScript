b16695839fb9b58aee46cfb0804b988216772cf7
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-01-18 12:20:48 +0530

    Anagram Detector

---
+++ 01 Masai/Revisit/DSA Context - 1/AnagramDetector.js
@@ -0,0 +1,7 @@
+function anagramDetector(S1,S2){
+    //write code here
+    S1 = S1.split("").filter((char) => char !== " ").map((char) => char.toLowerCase()).sort().join("");
+    S2 = S2.split("").filter((char) => char !== " ").map((char) => char.toLowerCase()).sort().join("");
+    console.log(S1===S2?"True":"False")
+    
+}
