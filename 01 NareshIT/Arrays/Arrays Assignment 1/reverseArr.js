e50e846ba60b05d5e8757c4993f4d124dd345025
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-04-17 22:22:05 +0530

    doubleArr

--- 01 NareshIT/Arrays/Arrays Assignment 1/reverseArr.js
+++ 01 NareshIT/Arrays/Arrays Assignment 1/reverseArr.js
@@ -48,3 +48,26 @@ function zeroPermutation(arr){
     return ans;
 }
 console.log(zeroPermutation([5,0,1,2,3,4]))
+
+
+/**
+ * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i]
+== nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+Input 1: nums = [1,2,1]
+Output 1: [1,2,1,1,2,1]
+Explanation 1: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+Input 2: nums = [1,3,2,1]
+Output 2: [1,3,2,1,1,3,2,1]
+Constraints:
+ 1 <= nums.length <= 1000
+ 1 <= nums[i] <= 103
+ */
+
+function doubleArr (arr){
+    return [...arr,...arr];
+}
+console.log(doubleArr([1,2,3,2,4]))
