d8710ff8d254055d98d1e8f94af8e5e15fda6a44
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-04-17 22:18:20 +0530

    zero permutation

--- 01 NareshIT/Arrays/Arrays Assignment 1/reverseArr.js
+++ 01 NareshIT/Arrays/Arrays Assignment 1/reverseArr.js
@@ -17,3 +17,34 @@ function reverseArr (arr){
 }
 
 console.log(reverseArr([1,2,3,4,5]))
+
+
+//Given a zero-based permutation nums (0-indexed), build an array ans of the same length where
+// ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1
+// (inclusive).
+
+// Input 1: nums = [0,2,1,5,3,4]
+// Output 1: [0,1,2,4,5,3]
+// Explanation 1: The array ans is built as follows:
+// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]],
+// nums[nums[5]]]
+// ans = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]] = [0,1,2,4,5,3]
+
+// Input 2: nums = [5,0,1,2,3,4]
+// Output 2: [4,5,0,1,2,3]
+
+// Constraints:
+//  1 <= nums.length <= 1000
+//  0 <= nums[i] <= nums.length
+//  The elements in nums are distinct.
+
+function zeroPermutation(arr){
+    let n = arr.length;
+    let ans = new Array(n)
+    for(let i=0;i<n;i++) {
+        ans[i] = arr[arr[i]]
+    }
+    return ans;
+}
+console.log(zeroPermutation([5,0,1,2,3,4]))
