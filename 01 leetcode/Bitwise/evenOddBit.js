7575ce9c4946f9f9c79ec082c7c3ae643cfe6209
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-07 15:58:39 +0530

    number of even and odd bits

---
+++ 01 leetcode/Bitwise/evenOddBit.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/number-of-even-and-odd-bits/
+var evenOddBit = function(n) {
+    let ans = [0,0];
+    let nBinary = n.toString(2);
+    let length = nBinary.length;
+    let bIdx = 0;
+    for(let i=length-1;i>=0;i--) {
+        if(nBinary[i]=== '1'){
+            if(bIdx%2===0){
+                ans[0]++;
+            }else{
+                ans[1]++;
+            }
+        }
+        bIdx++;
+    }
+    return ans;
+}
