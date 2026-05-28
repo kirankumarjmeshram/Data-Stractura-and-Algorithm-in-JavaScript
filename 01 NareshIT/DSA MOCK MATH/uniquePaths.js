74f64ecf9701ea8ed3d27acd970f1321730b8cb2
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-05-28 16:59:23 +0530

    Unique path using dp

--- 01 NareshIT/DSA MOCK MATH/uniquePaths.js
+++ 01 NareshIT/DSA MOCK MATH/uniquePaths.js
@@ -1,1 +1,10 @@
 // https://leetcode.com/problems/unique-paths/
+var uniquePaths = function(m, n) {
+    let dp = Array.from({length:m},()=>Array(n).fill(1));
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        }
+    }
+    return dp[m-1][n-1];
+};
