66513f5cf482da03baee15cbfabde4f8b7980602
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-06-07 19:34:07 +0530

    Update fib.js

--- recursion/fib.js
+++ recursion/fib.js
@@ -9,5 +9,22 @@ function fib(n){
     }
 }
 
+function fib2(n){
+    if(n<=0){
+        return 0;
+    }if(n==1||n==2){
+        return 1
+    }
+    let a = 0;
+    let b = 1
+    for(let i=2;i<=n;i++){
+        let temp = a+b;
+        a = b;
+        b= temp
+    }
+    return b
+
+}
+
 console.log(fib(10))//8
-console.log(fib(50,{}))//12586269025
+// console.log(fib2(50,{}))//12586269025
