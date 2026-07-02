e56024cf62c16d6d1fd96ff2e7fdeab66eb05648
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-02 08:08:56 +0530

    Refactor isAnagram and add one-hashmap solution
    Simplify the existing two-hashmap isAnagram implementation (use concise increment expressions and cleaner formatting) and add a new, more efficient single-hashmap implementation that counts characters from s and decrements while scanning t. Both implementations are included for reference; functionality is unchanged.

--- 01 NeetCode 150/Arrays and hashing/isAnagram.js
+++ 01 NeetCode 150/Arrays and hashing/isAnagram.js
@@ -1,36 +1,50 @@
 // https://leetcode.com/problems/valid-anagram/
 
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-var isAnagram = function(s, t) {
-        let n = s.length;
-        if(n !== t.length) return false;
-        let mapS = {};
-        let mapT = {};
-        for(let i=0;i<n;i++){
-            if(!mapS[s[i]]){
-                mapS[s[i]] = 1;
-            }else{
-                mapS[s[i]]++;
-            }
-        }
+// using 2 hashmap
+var isAnagram = function (s, t) {
+  let n = s.length;
+  if (n !== t.length) return false;
+  let mapS = {};
+  let mapT = {};
+  for (let i = 0; i < n; i++) {
+    mapS[s[i]] = (mapS[s[i]] || 0) +1
+    // if (!mapS[s[i]]) {
+    //   mapS[s[i]] = 1;
+    // } else {
+    //   mapS[s[i]]++;
+    // }
+  }
 
-        for(let i=0;i<n;i++){
-            if(!mapT[t[i]]){
-                mapT[t[i]] = 1;
-            }else{
-                mapT[t[i]]++;
-            }
-        }
+  for (let i = 0; i < n; i++) {
+    mapT[t[i]] = (mapT[t[i]] || 0) +1
+    // if (!mapT[t[i]]) {
+    //   mapT[t[i]] = 1;
+    // } else {
+    //   mapT[t[i]]++;
+    // }
+  }
 
-        for(let el of Object.keys(mapS)){
-            if(mapS[el] !== mapT[el]){
-                return false
-            }
-        }
-        return true;
-
+  for (let el of Object.keys(mapS)) {
+    if (mapS[el] !== mapT[el]) {
+      return false;
     }
+  }
+  return true;
+};
+
+// using one hashmap
+var isAnagram = function (s, t) {
+    let n = s.length;
+    if (n !== t.length) return false;
+    let map = {};
+    for (let el of s) {
+        map[el] = (map[el] || 0) +1
+    }
+
+    for (let el of t) {
+        if(!map[el]) return false;
+        map[el]--;
+    }
+
+    return true;
+};
