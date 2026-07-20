30ae0688816ef7193df21db1120c3a5d3a4c329f
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-20 10:30:24 +0530

    Create LL.js

---
+++ 01 NareshIT/LinkedList/LL.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Add at the beginning
+  addFirst(data) {
+    const newNode = new Node(data);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size++;
+  }
+
+  // Add at the end
+  addLast(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.size++;
+      return;
+    }
+
+    let temp = this.head;
+
+    while (temp.next !== null) {
+      temp = temp.next;
+    }
+
+    temp.next = newNode;
+    this.size++;
+  }
+
+  // Print the linked list
+  print() {
+    let temp = this.head;
+
+    while (temp !== null) {
+      process.stdout.write(`${temp.data} -> `);
+      temp = temp.next;
+    }
+
+    console.log("null");
+  }
+}
+
+const list = new LinkedList();
+list = addFirst(20);
+list = addFirst(20);
+list = addFirst(40);
+list = addFirst(50);
+
