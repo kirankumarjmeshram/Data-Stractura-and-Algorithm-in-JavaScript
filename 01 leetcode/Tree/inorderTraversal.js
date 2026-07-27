f3852edde790fc726ed1d36d152aa205f1c31fee
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-07-27 11:54:16 +0530

    Update inorderTraversal.js

--- 01 leetcode/Tree/inorderTraversal.js
+++ 01 leetcode/Tree/inorderTraversal.js
@@ -12,16 +12,15 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function(root) {
-    const result = [];
-    helper(root, result);
-    return result;
-     }
-    function helper(root, result) {
-        if(root!==null) {
-            helper(root.left, result)
-            result.push(root.val);
-            helper(root.right, result)
-    
-        }
-    };
+var inorderTraversal = function (root) {
+  const result = [];
+  helper(root, result);
+  return result;
+};
+function helper(root, result) {
+  if (root !== null) {
+    helper(root.left, result);
+    result.push(root.val);
+    helper(root.right, result);
+  }
+}
