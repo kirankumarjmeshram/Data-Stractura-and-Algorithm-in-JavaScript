190e99e82649540d8ca3ebe242a2557935fb4725
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-08-06 16:58:45 +0530

    Update isIsomorphic.js

--- 01 leetcode/Top Interview 150/Hashmap/isIsomorphic.js
+++ 01 leetcode/Top Interview 150/Hashmap/isIsomorphic.js
@@ -1,25 +1,27 @@
 // https://leetcode.com/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
 
-var isIsomorphic = function(s, t) {
-    let sMap = {};
-    let tMap = {};
-    let n = s.length;
-    let m = t.length;
-    if(n!==m) return false;
+var isIsomorphic = function (s, t) {
+  let sMap = {};
+  let tMap = {};
+  let n = s.length;
+  let m = t.length;
+  if (n !== m) return false;
 
-    for(let i=0;i<n;i++){
-        let sChar = s[i];
-        let tChar = t[i];
+  for (let i = 0; i < n; i++) {
+    let sChar = s[i];
+    let tChar = t[i];
 
-        if((sMap[sChar] && sMap[sChar] !== tChar) || (tMap[tChar] && tMap[tChar] !== sChar)) {
-            return false;
-        }
-        sMap[sChar] = tChar;
-        tMap[tChar] = sChar
-
+    if (
+      (sMap[sChar] && sMap[sChar] !== tChar) ||
+      (tMap[tChar] && tMap[tChar] !== sChar)
+    ) {
+      return false;
     }
-    return true;
+    sMap[sChar] = tChar;
+    tMap[tChar] = sChar;
+  }
+  return true;
 };
 
-console.log(isIsomorphic("egg", "add"))  // true
+console.log(isIsomorphic("egg", "add")); // true
 console.log(isIsomorphic("foo", "bar")); // false
