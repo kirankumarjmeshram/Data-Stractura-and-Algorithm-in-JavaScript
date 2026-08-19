764ea2e0ed96a1ff1e449a80349b70d7adfa3abb
Kirankumar J Meshram <kirankumar21895@gmail.com>
2026-08-19 17:50:56 +0530

    bfs od Adjacency Matrix and Adjacency List

--- graph/breadthFirstResearch.js
+++ graph/breadthFirstResearch.js
@@ -1,3 +1,30 @@
+//BFS of Adjacency Matrix
+// https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+let AdjacencyMatrix = [[2, 3, 1], [0], [0, 4], [0], [2]]
+function bfsAdj(adj) {
+        // code here
+    let ans = [0];
+    let visited = new Set();
+    visited.add(0)
+    let queue = [0];
+    while(queue.length){
+        let node = queue.shift();
+        for(let neighbour of adj[node]) {
+            if(!visited.has(neighbour)){
+                visited.add(neighbour);
+                ans.push(neighbour);
+                queue.push(neighbour);
+            }
+        }
+    }
+        return ans;
+}
+
+console.log(bfsAdj("Output: ",AdjacencyMatrix))
+
+//Output: [0, 2, 3, 1, 4]
+
+//Adjacency List
 const graph ={
     a:['c','b'],
     b:['d'],
@@ -19,11 +46,5 @@ const bfs=(graph,source)=>{
     }
     return ans;
 }
-console.log(bfs({
-    a:['c','b'],
-    b:['d'],
-    c:['e'],
-    d:['f'],
-    e:[],
-    f:[]
-},'a')); //[ 'a', 'c', 'b', 'e', 'd', 'f' ]
+console.log(bfs(graph,'a')); //[ 'a', 'c', 'b', 'e', 'd', 'f' ]
+
