ca2ba21eb1b9bb50ca778b62b1a2b9547cf60431
Kirankumar J Meshram <kirankumar21895@gmail.com>
2026-09-18 22:12:42 +0530

    Add deque-based constrainedSubsetSum solution
    Append an optimized monotonic-deque implementation of constrainedSubsetSum in Queues/constrainedSubsetSum.js. The new version computes dp using a sliding-window maximum (deque) to achieve O(n) time with O(n) space, while retaining the original DP approach above it. Returns the maximum dp value.

--- Queues/constrainedSubsetSum.js
+++ Queues/constrainedSubsetSum.js
@@ -11,3 +11,28 @@ var constrainedSubsetSum = function(nums, k) {
     }
     return Math.max(...dp);
 };
+
+// Queue Solution
+var constrainedSubsetSum = function(nums, k) {
+    let n = nums.length;
+    let dp = [...nums];
+
+    let deque = new Array();
+    let front = 0;
+    let back = 0;
+
+    for(let i=0; i<n;i++) {
+        while( front < back && deque[front] < i-k) {
+            front++;
+        }
+        if(front<back) {
+            dp[i] = Math.max(dp[i], nums[i]+dp[deque[front]]);
+        }
+        while(front < back && dp[deque[back-1]] <= dp[i]) {
+            back--;
+        }
+        deque[back++] = i;
+    }
+
+    return Math.max(...dp)
+};
